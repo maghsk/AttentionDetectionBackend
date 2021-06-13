28bae07003348ec47858910529afecff484c25ea
Han Yudong <maghsk2017@gmail.com>
2021-06-13 12:57:16 +0800

    fix cors issue

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template, jsonify
+from flask_cors import cross_origin, CORS
+
 import json
+import random
 import base64
 from recognition_camera import predict_expression, get_attention_score, get_user_photo
 app = Flask(__name__)
+# CORS(app, supports_credentials=True)
 capture, exp_model = predict_expression()
 
 NAME='Yudong'
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return "Hello World!" #render_template("index.html")
 
 @app.route('/data')
+@cross_origin()
 def get_data():
-    return json.dumps({'name':NAME,'value':get_attention_score(capture, exp_model)})
+    return jsonify({
+        'name':NAME,
+        # 'value':random.random()
+        'value': get_attention_score(capture, exp_model)
+    })
 
 @app.route('/camera')
+@cross_origin()
 def get_cam():
-    return json.dumps({'name':NAME,'value':base64.b64encode(get_user_photo(capture)).decode('utf-8')})
+    return jsonify({'name':NAME,'value':base64.b64encode(get_user_photo(capture)).decode('utf-8')})
 
 if __name__ == '__main__':
     app.run()
